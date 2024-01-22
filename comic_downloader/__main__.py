4e130812f1617cf1491f65709b880999be9995c3
Atronar <atronar@users.noreply.github.com>
2024-01-22 21:36:10 +0300

    Косметические правки

--- comic_downloader/__main__.py
+++ comic_downloader/__main__.py
@@ -11,20 +11,20 @@ if __name__ == '__main__':
 
     rss_list = get_db()
     procs=[]
-    
+
     # Sequantial Art
     procs.append(sp.run(f'python SAdownload.py {rss_list[0][4]} -folder "{rss_list[0][3]}"'))
     print("Процесс SA добавлен")
     if procs[0].returncode > rss_list[0][4]:
-        set_last_num(1,procs[0].returncode)
-        toaster.show_toast("RSS",f"Обновление: {rss_list[0][1]}")
+        set_last_num(1, procs[0].returncode)
+        toaster.show_toast("RSS", f"Обновление: {rss_list[0][1]}")
     else:
-        set_last_chk("1")
+        set_last_chk(1)
     print("Процесс SA завершён")
-    
+
     # Acomics
     for rss in rss_list[1:]:
-        #print('python acomicsdownload.py '+str(rss[1])+' '+str(rss[3])+' -folder "'+rss[2]+'" -desc '+rss[6]+' -imgtitle '+rss[7])
+        #print('python acomicsdownload.py ' + str(rss[1]) + ' ' + str(rss[3]) + ' -folder "' + rss[2] + '" -desc ' + rss[6] + ' -imgtitle ' + rss[7])
         try:
             desc = ' -desc' if str(rss[7])=='True' else ''
             imgtitle = ' -imgtitle' if str(rss[8])=='True' else ''
@@ -37,9 +37,9 @@ if __name__ == '__main__':
     for rss in rss_list[1:]:
         procs[rss[0]-1].wait()
         if procs[rss[0]-1].returncode > rss[4]:
-            set_last_num(rss[0],procs[rss[0]-1].returncode)
-            toaster.show_toast("RSS",f"Обновление: {rss[1]}")
+            set_last_num(rss[0], procs[rss[0]-1].returncode)
+            toaster.show_toast("RSS", f"Обновление: {rss[1]}")
         else:
-            set_last_chk(f"{rss[0]}")
+            set_last_chk(rss[0])
         print(f"Процесс {rss[1]} завершён")
 
