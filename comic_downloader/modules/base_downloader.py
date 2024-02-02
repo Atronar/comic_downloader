83b275331a09fc1a0e9f647e352b5193699219d7
Atronar <atronar@users.noreply.github.com>
2024-02-03 01:00:29 +0300

    Проблема с обрезаемыми системой конечными точками

--- comic_downloader/modules/base_downloader.py
+++ comic_downloader/modules/base_downloader.py
@@ -155,12 +155,12 @@ class BaseDownloader(ABC):
             'LST', 'KEYBD$', 'SCREEN$', '$IDLE$', 'CONFIG$'
         }
         if os.path.splitext(filename)[0].upper() in reserved_words: return f"__{filename}"
-        if set(filename)=={'.'}: return filename.replace('.', '\uff0e', 1)
+        if set(filename)=={'.'}: return filename.replace('.', '\uff0e')
         return "".join(
             chr(ord(c)+65248) if c in illegal_chars else c
             for c in filename
             if c not in illegal_unprintable
-        ).rstrip()
+        ).rstrip().rstrip('.')
 
     @abstractmethod
     def find_last(self) -> int:
